dfdc8eafb2c2f18ce2af9ebcc2fbc7756f817e0a
axlegames <axlegames>
2023-01-17 18:08:54 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 import About from "./screens/About";
 import Banner from "./components/Banner";
 import Giveaway from "./screens/Giveaway";
+import TSupply from "./screens/Tsupply";
 
 const Navbar = lazy(() => import("./components/navbar/Navbar"));
 const MobileNavbar = lazy(() => import("./components/navbar/MobileNavbar"));
@@ -124,6 +125,7 @@ export const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/giveaway" element={<Giveaway />} />
+            <Route path="/tsupply" element={<TSupply />} />
           </Routes>
         </Router>
       </ChakraProvider>
---
+++ src/screens/Tsupply.tsx
@@ -0,0 +1,7 @@
+import { Box} from "@chakra-ui/react";
+
+const TSupply = () => {
+    return (<Box>1000000000</Box>);
+  };
+  
+  export default TSupply;
