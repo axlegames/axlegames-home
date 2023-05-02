ba08c2e1649c855970e6fcb8f6699dc60fea6ab2
akashmadduru <akashmadduru@gmail.com>
2023-05-02 15:20:25 +0530

    minor fixes

--- src/abi/creds.tsx
+++ src/abi/creds.tsx
@@ -7,6 +7,7 @@ import StakingAbiMainnet from "./mainnet/AxleStakingMain.json";
 import TokenAbiTestnet from "../abi/testnet/AxleTokenTest.json";
 import PresaleAbiTestnet from "../abi/testnet/AxlePresaleTest.json";
 import StakingAbiTestnet from "../abi/testnet/AxleStakingTest.json";
+import FlexStakingTestnet from "../abi/testnet/AxleFlexStakingTest.json";
 
 interface Chain {
   chainName: string;
@@ -23,7 +24,8 @@ interface Creds {
   AXLE_CONTRACT: string;
   AXLE_ZEUS_PRESALE: string;
   AXLE_STAKING: string;
-  AXLE_SENDER: string;
+  AXLE_FLEX_STAKING: string;
+  flexStakingAbi: any;
   tokenAbi: any;
   presaleAbi: any;
   stakingAbi: any;
@@ -35,10 +37,11 @@ const mainnet: Creds = {
   AXLE_CONTRACT: "0x7c56C79a454CBFaf63BAdb39f82555109a2A80Bf",
   AXLE_ZEUS_PRESALE: "0xeA29a0f11EaAF0f88DBf705b2b53A09767cDF305",
   AXLE_STAKING: "0x20A33C553dB0b4AAD04065A234999C9228183291",
-  AXLE_SENDER: "",
+  AXLE_FLEX_STAKING: "",
   stakingAbi: StakingAbiMainnet,
   tokenAbi: TokenAbiMainnet,
   presaleAbi: PresaleAbiMainnet,
+  flexStakingAbi: null,
   isTestNet: false,
   chain: {
     chainName: "Smart Chain",
@@ -56,10 +59,11 @@ const testnet: Creds = {
   AXLE_CONTRACT: "0x3b12b9ec6a9f1514809eed63597c13ff6146aa08",
   AXLE_ZEUS_PRESALE: "0xaed66d62e70023762ba0977f5304116120706d84",
   AXLE_STAKING: "0xD996A74A3376D75812f07781EF22666323288B86",
-  AXLE_SENDER: "",
+  AXLE_FLEX_STAKING: "0xF637C152Be5aBcC7129F8E061938098604aa1507",
   stakingAbi: StakingAbiTestnet,
   tokenAbi: TokenAbiTestnet,
   presaleAbi: PresaleAbiTestnet,
+  flexStakingAbi: FlexStakingTestnet,
   isTestNet: true,
   chain: {
     chainName: "BSC Testnet",
@@ -78,7 +82,8 @@ const isDevServer = env.IS_DEV_SERVER;
 let creds: Creds = {
   AXLE_CONTRACT: "",
   AXLE_ZEUS_PRESALE: "",
-  AXLE_SENDER: "",
+  AXLE_FLEX_STAKING: "",
+  flexStakingAbi: null,
   AXLE_STAKING: "",
   presaleAbi: null,
   stakingAbi: null,
