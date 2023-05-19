c057ede9e92eb45585b7ba01bd25a76d4b4ab393
akashmadduru <akashmadduru@gmail.com>
2023-05-19 12:39:18 +0530

    minor fixes

--- src/Mint/Mint.tsx
+++ src/Mint/Mint.tsx
@@ -1,70 +1,171 @@
 import GIFI from "../assets/ticket.gif";
 import { brandingColors, brandingFonts } from "../config/brandingColors";
-import { Box, Flex, Image, Text, Button, Divider } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  Button,
+  Divider,
+  Input,
+} from "@chakra-ui/react";
 
-interface Props {
-  title: string;
-  sub: string;
-  number: number;
-}
+import { ethers } from "ethers";
 
-const Header = (props: Props) => {
-  return (
-    <Box
-      pos={"relative"}
-      rowGap={".2rem"}
-      display={"flex"}
-      flexDir="column"
-      px={8}
-      pb={props.number !== 3 ? 8 : 0}
-      borderLeft={
-        props.number === 0 || props.number === 1 ? "" : "1px dotted #ffffff"
-      }
-    >
-      <Text
-        left={"-14px"}
-        pos={"absolute"}
-        color={brandingColors.primaryButtonColor}
-        fontFamily={brandingFonts.headingFont}
-        bg={brandingColors.bgColor}
-        boxShadow="dark-lg"
-        border={
-          props.number !== 0 ? `1px solid ${brandingColors.neuPrimaryBg}` : ""
-        }
-        px={2}
-        borderRadius="xl"
-        py={props.number !== 0 ? 1 : 0}
-      >
-        {props.number !== 0 ? props.number : null}
-      </Text>
-      <Text fontSize={{ base: "2xl" }} fontFamily={brandingFonts.headingFont}>
-        {props.title}
-      </Text>
-      <Text
-        fontSize={"md"}
-        fontWeight={"bold"}
-        fontFamily={brandingFonts.readingFont}
-      >
-        {props.sub}
-      </Text>
-    </Box>
-  );
-};
+import { web3Modal, chainIds } from "../Staking/components/utils";
+
+import creds from "../abi/creds";
+import { useState } from "react";
+
+import Wallet from "../Staking/Wallet";
+import Logo from "../assets/logo.png";
+
+const TOKEN_CONTRACT_ADDRESS = creds.AXLE_CONTRACT;
+const axleTokenABI = creds.tokenAbi;
 
 const Mint = () => {
+  const [balance, setBalance] = useState(0);
+  const [axleBalance, setAxleBalance] = useState<any>("0");
+
+  const [address, setAddress] = useState<string>("");
+  const [onChain, setOnChain] = useState("");
+  const [openWallet, setOpenWallet] = useState(false);
+
+  const [tokenContract, setTokenContract] = useState<any>();
+
+  console.log(axleBalance, onChain, tokenContract);
+
+  const setNetworkName = (chainId: number) => {
+    for (let i = 0; i < chainIds.length; i++) {
+      if (chainIds[i].chainId === chainId) {
+        setOnChain(chainIds[i].network);
+      }
+    }
+  };
+
+  const switchNetwork = async () => {
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: ethers.utils.hexlify(creds.chain.chainId) }],
+      });
+    } catch (err: any) {
+      // This error code indicates that the chain has not been added to MetaMask
+      if (err.code === 4902) {
+        await window.ethereum.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainName: creds.chain.chainName,
+              chainId: creds.chain.chainId,
+              nativeCurrency: {
+                name: creds.chain.nativeCurrency.name,
+                decimals: creds.chain.nativeCurrency.decimals,
+                symbol: creds.chain.nativeCurrency.symbol,
+              },
+              rpcUrls: creds.chain.rpcUrls,
+            },
+          ],
+        });
+      }
+    }
+  };
+
+  const connectWeb3Wallet = async () => {
+    try {
+      const e9 = 10 ** 9;
+      const web3Provider = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(web3Provider);
+      const web3Accounts = await provider.listAccounts();
+      const network = await provider.getNetwork();
+      if (network.chainId !== creds.chain.chainId) switchNetwork();
+      let bnbBal: any = await provider.getBalance(web3Accounts[0]);
+      bnbBal = Number(ethers.utils.formatEther(bnbBal));
+      const signer = provider.getSigner();
+      const token = new ethers.Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        axleTokenABI,
+        signer
+      );
+      let bal = await token.balanceOf(web3Accounts[0]);
+      bal = ethers.utils.formatEther(bal);
+      setAddress(web3Accounts[0]);
+      setNetworkName(network.chainId);
+      setTokenContract(token);
+      setAxleBalance(bal * e9);
+      setBalance(bnbBal);
+      localStorage.setItem("isWalletConnected", "true");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const disconnectWeb3Modal = async (loaded: boolean = false) => {
+    web3Modal.clearCachedProvider();
+    if (!loaded) window.location.reload();
+  };
+
+  const [form, setForm] = useState([
+    { k: 0, v: "" },
+    { k: 1, v: "" },
+    { k: 2, v: "" },
+    { k: 3, v: "" },
+    { k: 4, v: "" },
+    { k: 5, v: "" },
+  ]);
+
+  const updateForm = (e: any, i: number) => {
+    let tform = form;
+    tform[i].v = e.target.value;
+    setForm([...tform]);
+  };
+
+  const generateNumber = () => {
+    const num = String(Math.floor(100000 + Math.random() * 900000));
+    let tform = form;
+    for (let i = 0; i < num.length; i++) tform[i].v = num[i];
+    console.log(tform);
+    setForm([...tform]);
+  };
+
   return (
-    <Box bg={brandingColors.bgColor}>
+    <Box
+      bg={brandingColors.bgColor}
+      backgroundImage={`https://axlegames.s3.ap-south-1.amazonaws.com/theme_assets/images/how-works-bg.png`}
+      backgroundSize="contain"
+      py={6}
+      minH="100vh"
+    >
+      <Box
+        alignItems={"center"}
+        mx={24}
+        display={"flex"}
+        justifyContent={{ base: "center", md: "space-between" }}
+        my={{ base: 8, md: 4 }}
+      >
+        <Image maxH={"8vh"} src={Logo}></Image>
+        <Wallet
+          address={address}
+          disconnect={disconnectWeb3Modal}
+          balance={balance}
+          connectWallet={connectWeb3Wallet}
+          isLoading={false}
+          openWallet={openWallet}
+          setOpenWallet={setOpenWallet}
+        />
+      </Box>
+
       <Box
         width={{ base: "100%", md: "90%" }}
         mx="auto"
-        p={{ base: 2, lg: 12 }}
+        p={{ base: 8, lg: 12 }}
         columnGap="6rem"
+        rowGap={"3rem"}
         display={"flex"}
-        bg={brandingColors.bgColor}
         justifyContent="space-evenly"
+        flexDir={{ base: "column", lg: "row" }}
       >
         <Box
-          alignItems={"center"}
           display={"flex"}
           flexDir="column"
           justifyContent={"center"}
@@ -123,28 +224,39 @@ const Mint = () => {
           rowGap="2rem"
           flexDir={"column"}
           borderRadius={"xl"}
-          p={8}
-          bg={brandingColors.newHighlightColor}
+          px={2}
+          py={8}
           justifyContent="center"
           alignItems={"center"}
+          width="100%"
         >
-          <Text fontSize={{ base: "2xl" }} fontFamily={brandingFonts.subFont}>
+          <Text
+            textAlign={"center"}
+            fontSize={{ base: "xl", sm: "2xl" }}
+            fontFamily={brandingFonts.subFont}
+          >
             Mint your ticket to reveal your NFT
           </Text>
-          <Image src={GIFI} />
-          <Flex width={"100%"} px={8} justifyContent={"space-between"}>
+          <Image borderRadius={"xl"} src={GIFI} />
+          <Flex
+            width={"100%"}
+            p={{ base: 2, lg: 6 }}
+            borderRadius="xl"
+            justifyContent={"space-between"}
+            boxShadow={"dark-lg"}
+          >
             <Box>
               <Text
-                fontSize={{ base: "xl" }}
-                fontFamily={brandingFonts.readingFont}
+                fontSize={{ base: "sm", md: "xl" }}
+                fontFamily={brandingFonts.subFont}
                 fontWeight="bold"
               >
                 DCB World x BRITTO
               </Text>
               <Text
                 fontWeight="bold"
-                fontSize={{ base: "xl" }}
-                fontFamily={brandingFonts.readingFont}
+                fontSize={{ base: "sm", md: "xl" }}
+                fontFamily={brandingFonts.subFont}
               >
                 Edition #000001
               </Text>
@@ -152,16 +264,18 @@ const Mint = () => {
             <Box>
               <Text
                 fontWeight="bold"
-                fontSize={{ base: "xl" }}
-                fontFamily={brandingFonts.readingFont}
+                fontSize={{ base: "sm", md: "xl" }}
+                fontFamily={brandingFonts.subFont}
                 color={brandingColors.primaryButtonColor}
+                bg={brandingColors.bgColor}
+                boxShadow="dark-lg"
               >
                 Price
               </Text>
               <Text
                 fontWeight="bold"
-                fontSize={{ base: "xl" }}
-                fontFamily={brandingFonts.readingFont}
+                fontSize={{ base: "sm", md: "xl" }}
+                fontFamily={brandingFonts.subFont}
                 color={brandingColors.primaryButtonColor}
               >
                 0.2
@@ -175,8 +289,9 @@ const Mint = () => {
             flexDir="column"
           >
             <Text
+              textAlign={"center"}
               fontWeight={"normal"}
-              fontSize={{ base: "lg" }}
+              fontSize={{ base: "sm", md: "xl", lg: "md" }}
               fontFamily={brandingFonts.subFont}
               color={brandingColors.secondaryTwoTextColor}
             >
@@ -184,28 +299,33 @@ const Mint = () => {
             </Text>
             <Box>
               <Flex my={3} columnGap={".5rem"}>
-                {[4, 6, 8, 5, 3].map((m, i) => (
-                  <Box
-                    borderRadius={"xl"}
+                {form.map((m, i) => (
+                  <Input
+                    key={i}
                     py={2}
                     px={3}
+                    value={m.v}
+                    fontSize={"xl"}
+                    fontFamily={brandingFonts.subFont}
+                    color={brandingColors.primaryButtonColor}
+                    variant={"unstyled"}
                     bg={brandingColors.bgColor}
-                    key={i}
-                  >
-                    <Text
-                      fontSize={"xl"}
-                      fontFamily={brandingFonts.subFont}
-                      color={brandingColors.primaryButtonColor}
-                    >
-                      {m}
-                    </Text>
-                  </Box>
+                    borderRadius={"xl"}
+                    outline="none"
+                    border="none"
+                    boxShadow={"dark-lg"}
+                    textAlign="center"
+                    maxW="12"
+                    maxLength={1}
+                    onChange={(e) => updateForm(e, i)}
+                  />
                 ))}
               </Flex>
             </Box>
             <Text
               fontWeight={"normal"}
-              fontSize={{ base: "md" }}
+              textAlign="center"
+              fontSize={{ base: "sm", md: "xl", lg: "md" }}
               fontFamily={brandingFonts.subFont}
             >
               This ticket number is already sold. Choose a different number.
@@ -239,24 +359,28 @@ const Mint = () => {
               </Text>
             </Box>
             <Text
-              fontSize={"2xl"}
+              fontSize={{ base: "sm", md: "xl" }}
               fontFamily={brandingFonts.subFont}
               color={brandingColors.secondaryTextColor}
               mb={4}
               cursor="pointer"
+              onClick={generateNumber}
             >
               GENERATE RANDOM TICKET
             </Text>
-            <Button
-              fontWeight={"normal"}
-              fontSize={{ base: "2xl" }}
-              fontFamily={brandingFonts.subFont}
-              color={brandingColors.primaryButtonColor}
-              bg={brandingColors.bgColor}
-              boxShadow="dark-lg"
-            >
-              Connect wallet
-            </Button>
+            {address === "" ? (
+              <Button
+                fontWeight={"normal"}
+                fontSize={{ base: "2xl" }}
+                fontFamily={brandingFonts.subFont}
+                color={brandingColors.primaryButtonColor}
+                bg={brandingColors.bgColor}
+                boxShadow="dark-lg"
+                onClick={connectWeb3Wallet}
+              >
+                Connect wallet
+              </Button>
+            ) : null}
           </Box>
         </Box>
       </Box>
@@ -265,3 +389,55 @@ const Mint = () => {
 };
 
 export default Mint;
+
+interface Props {
+  title: string;
+  sub: string;
+  number: number;
+}
+
+const Header = (props: Props) => {
+  return (
+    <Box
+      pos={"relative"}
+      rowGap={".2rem"}
+      display={"flex"}
+      flexDir="column"
+      px={8}
+      pb={props.number !== 3 ? 8 : 0}
+      borderLeft={
+        props.number === 0 || props.number === 1 ? "" : "1px dotted #ffffff"
+      }
+    >
+      <Text
+        left={"-14px"}
+        pos={"absolute"}
+        color={brandingColors.primaryButtonColor}
+        fontFamily={brandingFonts.headingFont}
+        bg={brandingColors.bgColor}
+        boxShadow="dark-lg"
+        border={
+          props.number !== 0 ? `1px solid ${brandingColors.neuPrimaryBg}` : ""
+        }
+        px={2}
+        borderRadius="xl"
+        py={props.number !== 0 ? 1 : 0}
+      >
+        {props.number !== 0 ? props.number : null}
+      </Text>
+      <Text
+        fontSize={{ base: "lg", lg: "2xl" }}
+        fontFamily={brandingFonts.headingFont}
+      >
+        {props.title}
+      </Text>
+      <Text
+        fontSize={{ base: "sm", lg: "md" }}
+        fontWeight={"bold"}
+        fontFamily={brandingFonts.readingFont}
+      >
+        {props.sub}
+      </Text>
+    </Box>
+  );
+};
